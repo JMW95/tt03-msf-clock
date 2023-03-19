d01fd7c06a88cc9bec9331a0738d115f1860765e
Jamie Wood <jamie.wood70@ntlworld.com>
2023-03-19 12:45:02 +0000

    Fix signal names in cocotb test bench

--- src/test.py
+++ src/test.py
@@ -5,16 +5,16 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
 async def test_tmp(dut):
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk_i, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    dut.rst.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst.value = 0
+    dut.rst_i.value = 1
+    await ClockCycles(dut.clk_i, 10)
+    dut.rst_i.value = 0
 
-    prev = dut.segments.value
-    await ClockCycles(dut.clk, 1)
+    prev = dut.outputs_o.value
+    await ClockCycles(dut.clk_i, 1)
 
     for i in range(10):
-        await ClockCycles(dut.clk, 1)
-        assert int(dut.segments.value) == (not prev)
+        await ClockCycles(dut.clk_i, 1)
+        assert int(dut.outputs_o.value) == (not prev)

aec5421946bf0ae18a2b88bf8342949330f109be
Jamie Wood <jamie.wood70@ntlworld.com>
2023-03-19 16:01:17 +0000

    Add option to run gate-level tests locally

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -eux
 
 . ./env
 
+# Install apt deps
 if ! which iverilog; then
     sudo apt install iverilog verilator
 fi
@@ -11,6 +12,15 @@ fi
 # Install python deps
 if ! which cocotb-config; then
     pip install -qr requirements.txt
+fi
+
+# Run with `gate` as first argument to run a gate-level simulation instead
+TYPE=${1:-normal}
+
+export GATES="no"
+if [ "x$TYPE" = "xgate" ]; then
+    export GATES="yes"
+    cp runs/wokwi/results/final/verilog/gl/jmw95_top.v test/test_top/gate_level_netlist.v
 fi
 
 # Run tests

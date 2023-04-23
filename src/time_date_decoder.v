ac1b75ebf53814b957147c3ac22c471554295b2d
Jamie Wood <jamie.wood70@ntlworld.com>
2023-04-23 22:39:16 +0100

    Add sanity checks on time/date BCD values

--- src/time_date_decoder.v
+++ src/time_date_decoder.v
@@ -32,11 +32,18 @@ reg [59:54] b_shift_reg;
 
 reg valid_reg;
 
+wire year_ok = year_h_o <= 9 && year_l_o <= 9;
+wire month_ok = (month_h_o == 0 && month_l_o <= 9 && month_l_o > 0) || (month_h_o == 1 && month_l_o <= 2);
+wire day_ok = (day_h_o <= 2 && day_l_o <= 9) || (day_h_o == 3 && day_l_o <= 1);
+wire hour_ok = (hour_h_o < 2 && hour_l_o <= 9) || (hour_h_o == 2 && hour_l_o <= 3);
+wire minute_ok = minute_h_o <= 5 && minute_l_o <= 9;
+
 wire parity_54b = b_shift_reg[54] ^ (^a_shift_reg[24:17]);
 wire parity_55b = b_shift_reg[55] ^ (^a_shift_reg[35:25]);
 wire parity_56b = b_shift_reg[56] ^ (^a_shift_reg[38:36]);
 wire parity_57b = b_shift_reg[57] ^ (^a_shift_reg[51:39]);
 wire top_of_minute = a_shift_reg[59:52] == 8'b01111110;
+wire time_ok = year_ok & month_ok & day_ok & hour_ok & minute_ok;
 wire parity_valid = parity_54b & parity_55b & parity_56b & parity_57b & top_of_minute;
 
 function [3:0] swap4(input [3:0] a);
@@ -68,7 +75,7 @@ assign hour_h_o = swap2(a_shift_reg[40:39]);
 assign hour_l_o = swap4(a_shift_reg[44:41]);
 assign minute_h_o = swap3(a_shift_reg[47:45]);
 assign minute_l_o = swap4(a_shift_reg[51:48]);
-assign valid_o = parity_valid && bits_is_second_00_i && !valid_reg;
+assign valid_o = parity_valid && bits_is_second_00_i && time_ok && !valid_reg;
 
 always @(posedge clk_i) begin
     // Shift data in
@@ -77,7 +84,7 @@ always @(posedge clk_i) begin
         b_shift_reg <= { bits_data_i[1], b_shift_reg[59:55] };
     end
 
-    if (parity_valid && bits_is_second_00_i) begin
+    if (parity_valid && bits_is_second_00_i && time_ok) begin
         if (!valid_reg) begin
             valid_reg <= 1;
         end
